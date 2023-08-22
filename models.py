5c832e1a5fa918850b38933ca74158b65fb936e6
auto <83151966+AutoHDR@users.noreply.github.com>
2023-08-22 09:39:33 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,17 +1,12 @@
 import math
-# from inplace_abn import InPlaceABN
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from vgg_model import vgg19
-
 from torch.nn import Parameter
-
-
 
 class DoubleConv(nn.Module):
     """(convolution => [BN] => ReLU) * 2"""
-
     def __init__(self, in_channels, out_channels, mid_channels=None):
         super().__init__()
         if not mid_channels:
@@ -24,7 +19,6 @@ class DoubleConv(nn.Module):
             nn.BatchNorm2d(out_channels),
             nn.LeakyReLU(0.1, True)
         )
-
     def forward(self, x):
         x = self.double_conv(x)
         return x
@@ -32,40 +26,29 @@ class DoubleConv(nn.Module):
 
 class DownDC(nn.Module):
     """Downscaling with stride conv then double conv"""
-
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.main = nn.Sequential(
             nn.Conv2d(in_channels, in_channels, 4, 2, 1),
             nn.LeakyReLU(0.1, True),
             DoubleConv(in_channels, out_channels)
-            # ResBlock(in_channels, out_channels)
-        )
-        
-
+        )    
     def forward(self, x):
-
         x = self.main(x)
-
         return x
 
-  
 class SDFT(nn.Module):
-
     def __init__(self, norm_dim, channels, kernel_size = 3):
         super().__init__()
-        
         # generate global conv weights
         fan_in = channels * kernel_size ** 2
         self.kernel_size = kernel_size
         self.padding = kernel_size // 2
-
         self.scale = 1 / math.sqrt(fan_in)
         self.modulation = nn.Conv2d(norm_dim, channels, 1)
         self.weight = nn.Parameter(
             torch.randn(1, channels, channels, kernel_size, kernel_size)
         )
-
     def forward(self, fea, norm_feat):
         # for global adjustation
         B, C, H, W = fea.size()
@@ -75,64 +58,46 @@ class SDFT(nn.Module):
         # demodulation
         demod = torch.rsqrt(weight.pow(2).sum([2, 3, 4]) + 1e-8)
         weight = weight * demod.view(B, C, 1, 1, 1)
-
         weight = weight.view(
             B * C, C, self.kernel_size, self.kernel_size
         )
-
         fea = fea.view(1, B * C, H, W)
         fea = F.conv2d(fea, weight, padding=self.padding, groups=B)
         fea = fea.view(B, C, H, W)
-
         return fea
 
-
-
 class UpBlock(nn.Module):
-    
-
     def __init__(self, normfeat_dim, in_channels, out_channels, kernel_size = 3, bilinear=True):
         super().__init__()
-
         # if bilinear, use the normal convolutions to reduce the number of channels
         if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
-            
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)     
         else:
             self.up = nn.ConvTranspose2d(in_channels , in_channels // 2, kernel_size=2, stride=2)
-
         self.conv_cat = nn.Sequential(
             nn.Conv2d(in_channels // 2 + in_channels // 8, out_channels, 1, 1, 0),
             nn.LeakyReLU(0.2, True),
             nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
             nn.LeakyReLU(0.2, True)
         )
-
         self.conv_s = nn.Conv2d(in_channels//2, out_channels, 1, 1, 0)
-
         # generate global conv weights
         self.SDFT = SDFT(normfeat_dim, out_channels, kernel_size)
-
 
     def forward(self, x1, x2, normal_feat):
         x1 = self.up(x1)
         x1_s = self.conv_s(x1)
-
         x = torch.cat([x1, x2[:, ::4, :, :]], dim=1)
         x = self.conv_cat(x)
         x = self.SDFT(x, normal_feat)
-
         x = x + x1_s
-
         return x
     
 class NormalEncoder(nn.Module):
     def __init__(self, norm_dim=512):
         super(NormalEncoder, self).__init__()
-
         # self.vgg = vgg19(pretrained_path=None)
         self.vgg =  vgg19(pretrained_path = '/home/xteam/PaperCode/data_zoo/vgg19-dcbb9e9d.pth', require_grad = False)
-
         self.feature2vector = nn.Sequential(
             nn.Conv2d(norm_dim, norm_dim, 4, 2, 2), # 8x8
             nn.LeakyReLU(0.2, True),
@@ -144,7 +109,6 @@ class NormalEncoder(nn.Module):
             nn.Conv2d(norm_dim, norm_dim, 3, 1, 1),
             nn.LeakyReLU(0.2, True),
 
-
             nn.AdaptiveAvgPool2d((1, 1)), # 1x1
 
             nn.Conv2d(norm_dim, norm_dim//2, 1), # linear-1
@@ -155,15 +119,12 @@ class NormalEncoder(nn.Module):
             nn.Conv2d(norm_dim//2, norm_dim, 1), # linear-3
         )
         # d = 512
-
     def forward(self, x):
         # x #[0, 1] RGB
         b, c, w, h = x.shape
         vgg_fea = self.vgg(x.expand([b, 3, w, h]), layer_name='relu5_2') # [B, 512, 16, 16]
         normal_feat = self.feature2vector(vgg_fea[-1]) # [B, 512, 1, 1]
-
         return normal_feat
-
 
 class NormalDecoder(nn.Module):
     ### this model output is ab
